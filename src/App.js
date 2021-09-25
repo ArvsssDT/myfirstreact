2eb39dbd37a6eec45eb3cb97dae141d488bc7869
ArvsssDT <82487903+ArvsssDT@users.noreply.github.com>
2021-09-25 16:15:00 +0800

    Update

--- src/App.js
+++ src/App.js
@@ -5,19 +5,33 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="https://static.wikia.nocookie.net/kpop/images/f/f2/Girls_Planet_999_logo.png/revision/latest/scale-to-width-down/400?cb=20210905230415" className="App-logo" alt="logo" />
-        <p>
-        <h1>Nood tayo ng GP 999.</h1> <h3> Please Vote Kim Bo Ra(김보라)</h3>
-        </p>
+      
+        &nbsp;&nbsp;&nbsp;
         
-        <a
+        <img src="https://preview.redd.it/zgm7refzy4i71.jpg?width=400&format=pjpg&auto=webp&s=4003130d67dec9fe2264ef3b02a36cfd1d32a6b2" className="App-logo" alt="logo" />
+        <p>
+        <h1>Let's 
+          <a
           className="App-link"
-          href="https://www.iq.com/play/girls-planet-999-12199gfpr0x?frmrp=home&frmb=R:37824992012&frmrs=1"
+          href="https://www.iq.com/play/girls-planet-999-2021-08-06-o1ewubywr0?frmrp=home&frmb=R:37824992012&frmrs=1"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Watch Girls Planet 999 here
-        </a>
+          > Watch GP 999</a>!😄</h1>
+        </p>
+
+        <iframe width="852" height="480" src="https://www.youtube.com/embed/-BJtXwBt8YY" title="YouTube video player"
+        frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+         </iframe>
+         
+        &nbsp;
+        
+         <h3> Please Vote Kim Bo Ra(김보라)🙏</h3>
+        <iframe width="852" height="480" src="https://www.youtube.com/embed/gd4X22ZvYzQ" title="YouTube video player" 
+        frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+        </iframe>
+        
+         &nbsp;
+         
       </header>
     </div>
   );
