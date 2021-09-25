f7d5ef443ea119a98be97d1bf673601d16027db4
ArvsssDT <82487903+ArvsssDT@users.noreply.github.com>
2021-09-25 15:37:38 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://static.wikia.nocookie.net/kpop/images/f/f2/Girls_Planet_999_logo.png/revision/latest?cb=20210905230415" className="App-logo" alt="logo" />
         <p>
         <h1>Nood tayo ng GP 999.</h1> <h2> Please Vote Kim Bo Ra(김보라)</h2>
         </p>
