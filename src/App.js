351f34536482a86770543fee5faf624dc08c7f68
ArvsssDT <82487903+ArvsssDT@users.noreply.github.com>
2021-09-25 15:24:36 +0800

    Update

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" border-radius: 50% alt="logo">
         <p>
            Nood tayo ng GP 999. Please Vote Kim Bo Ra(김보라)
         </p>
-        
-        <iframe width="420" height="315"
-            src="https://www.youtube.com/watch?v=-BJtXwBt8YY">
-        </iframe>
-        
+
+        <iframe width="1280" height="720" src="https://www.youtube.com/embed/-BJtXwBt8YY" title="YouTube video player" 
+        frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+         </iframe>
+         
         <a
           className="App-link"
           href="https://www.iq.com/play/girls-planet-999-12199gfpr0x?frmrp=home&frmb=R:37824992012&frmrs=1"
