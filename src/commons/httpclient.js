23a0225087a89cfe080a0e393a0eeed237b150ba
ArvsssDT <82487903+ArvsssDT@users.noreply.github.com>
2021-12-15 09:41:32 +0800

    Update httpclient.js

--- src/commons/httpclient.js
+++ src/commons/httpclient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'http://localhost:8080/api/v1',
+    baseURL: 'http://localhost:8080/api/v1', //https://ics2608-myfirstreact.herokuapp.com/myfirstreact - Cant use ClearDB MYSQl( heroku wont accept my credit and debit cards :(( )
     headers:{
         'content-Type': 'application/json'
     }
